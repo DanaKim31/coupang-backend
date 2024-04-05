89a61b8047158f072749e6cf81681da777af2b41
DanaKim31 <152463101+DanaKim31@users.noreply.github.com>
2024-04-05 15:35:37 +0900

    SpringBoot coupang_댓글, 대댓글 실습(Review)

--- src/main/java/com/kh/coupang/controller/ReviewController.java
+++ src/main/java/com/kh/coupang/controller/ReviewController.java
@@ -1,9 +1,11 @@
 package com.kh.coupang.controller;
 
 import com.kh.coupang.domain.*;
+import com.kh.coupang.service.ReviewCommentService;
 import com.kh.coupang.service.ReviewService;
 import com.querydsl.core.BooleanBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
@@ -12,6 +14,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,6 +25,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,6 +36,9 @@ public class ReviewController {
 
     @Autowired
     private ReviewService service;
+
+    @Autowired
+    private ReviewCommentService commentService;
 
     @Value("${spring.servlet.multipart.location}")
     private String uploadPath;
@@ -81,4 +90,69 @@ public class ReviewController {
         return ResponseEntity.status(HttpStatus.OK).body(list.getContent());
     }
 
+
+    // 리뷰 댓글 추가
+    @PostMapping("/review/comment")
+    public ResponseEntity createComment(@RequestBody ReviewComment vo) {
+
+        Object principal = authentication();
+
+        if(principal instanceof User) {
+            User user = (User) principal;
+            vo.setUser(user);
+            return ResponseEntity.ok(commentService.create(vo));
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
+
+    // 리뷰 1개에 따른 댓글 조회
+    @GetMapping("/public/review/{code}/comment")
+    public ResponseEntity<List<ReviewCommentDTO>> viewComment(@PathVariable(name="code") int code) {
+        List<ReviewComment> topList = commentService.getTopComments(code);
+        List<ReviewCommentDTO> response = new ArrayList<>();
+
+        for(ReviewComment top : topList) {
+            List<ReviewComment> replies = commentService.getReplyComments(top.getReviComCode(), code);
+            List<ReviewCommentDTO> repliesDTO = new ArrayList<>();
+
+            for(ReviewComment reply : replies) {
+                ReviewCommentDTO dto = ReviewCommentDTO.builder()
+                        .reviCode(reply.getReviCode())
+                        .reviComCode(reply.getReviComCode())
+                        .reviComDesc(reply.getReviComDesc())
+                        .reviComDate(reply.getReviComDate())
+                        .user(UserDTO.builder()
+                                .id(reply.getUser().getId())
+                                .name(reply.getUser().getName())
+                                .build())
+                        .build();
+                repliesDTO.add(dto);
+            }
+
+            ReviewCommentDTO dto = ReviewCommentDTO.builder()
+                    .reviCode(top.getReviCode())
+                    .reviComCode(top.getReviComCode())
+                    .reviComDesc(top.getReviComDesc())
+                    .reviComDate(top.getReviComDate())
+                    .user(UserDTO.builder()
+                            .id(top.getUser().getId())
+                            .name(top.getUser().getName())
+                            .build())
+                    .replies(repliesDTO)
+                    .build();
+            response.add(dto);
+
+        }
+
+        return ResponseEntity.ok(response);
+    }
+
+
+    public Object authentication() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        return authentication.getPrincipal();
+    }
 }
